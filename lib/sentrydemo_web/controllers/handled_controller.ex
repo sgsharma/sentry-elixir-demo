a36156726f8f8134ad3743d9090ac42e5c8e3124
sgsharma <sasha.s.sharma@gmail.com>
2020-04-09 19:26:31 -0700

    updated the handled controller

--- lib/sentrydemo_web/controllers/handled_controller.ex
+++ lib/sentrydemo_web/controllers/handled_controller.ex
@@ -2,8 +2,10 @@ defmodule SentrydemoWeb.HandledController do
     use SentrydemoWeb, :controller
 
     def index(conn, _params) do
+        msg = %{"Error" => "A handled error occurred."}
         conn
-        |> send_resp(500, Poison.encode!(%{"Error" => "A handled error occurred."}))
+        |> send_resp(500, Poison.encode!(msg))
         |> halt
+        raise "#{msg["Error"]}"
     end
   end
